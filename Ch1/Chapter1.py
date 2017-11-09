5f016439830cee65b41e386d531ac4d83cbe123a
Gordon Olwell <gdolwell@gmail.com>
2017-11-09 15:39:24 -0600

    Chapter 1 completed

---
+++ Ch1/Chapter1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Chapter 1 Solutions to Think Python by Robert Downey
+Gordon Olwell
+November 9, 2017
+"""
+
+print("Hello World")
+
+#1
+t = 42*60+42
+Tinhours = (((42*60)+42)/60/60)
+#2
+DinMiles = 10*1.61
+#3
+DinMiles /  Tinhours
+
